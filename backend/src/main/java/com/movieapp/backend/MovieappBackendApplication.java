b00761be596afdc95c9a7efe907f6878d323717a
mrattcs <j.t.k.hashemi@gmail.com>
2025-08-06 03:07:12 +0200

    initalizing project

---
+++ backend/src/main/java/com/movieapp/backend/MovieappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movieapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieappBackendApplication.class, args);
+	}
+
+}
